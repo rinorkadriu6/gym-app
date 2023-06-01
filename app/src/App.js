b90568e122afe1fdb20eb8633258d0600677dc24
Rinor Kadriu <rinorkadriu6@gmail.com>
2023-06-02 00:35:15 +0200

    toastify

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
-import { Badge, Nav } from "react-bootstrap";
+import { Badge, Nav, NavDropdown } from "react-bootstrap";
 import { useContext } from "react";
 import { Store } from "./Store";
 import CartScreen from "./screens/CartScreen";
 import SignInScreen from "./screens/SignInScreen";
 
 function App() {
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+  const signoutHandler = () => {
+    ctxDispatch({ type: "USER_SIGNOUT" });
+    localStorage.removeItem("userInfo");
+  };
 
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1}/>
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -25,7 +32,7 @@ function App() {
                 <Navbar.Brand>Amazon</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
-                <Link to='/cart' className="nav-link">
+                <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
@@ -33,6 +40,28 @@ function App() {
                     </Badge>
                   )}
                 </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,23 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Rinor',
+            email: 'admin@test.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true
+        },
+        {
+            name: 'John',
+            email: 'john@test.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false
+        }
+    ],
     products: [
         {
-            _id: '1',
+            // _id: '1',
             name: 'Nike Slim shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
@@ -14,7 +30,7 @@ const data = {
             description: "high quality shirt"
         },
         {
-            _id: '2',
+            // _id: '2',
             name: 'Nike Slim pants',
             slug: 'nike-slim-pants',
             category: 'Pants',
@@ -27,7 +43,7 @@ const data = {
             description: "high quality pants"
         },
         {
-            _id: '3',
+            // _id: '3',
             name: 'Adidas fit joggers',
             slug: 'adidas-fit-joggers',
             category: 'pants',
@@ -40,7 +56,7 @@ const data = {
             description: "high quality pants"
         },
         {
-            _id: '4',
+            // _id: '4',
             name: 'Adidas fit pants',
             slug: 'adidas-fit-pants',
             category: 'pants',
